4cc0b5bcfafb5824c6de7ca6d447ddc2d03ebddf
Khizar khan <khizarazizktk@gmail.com>
2026-09-24 16:01:43 +0500

    configure backend for vercel

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,8 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/profile", profileRoutes);
 
 // Start the server.
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+module.exports = app;
